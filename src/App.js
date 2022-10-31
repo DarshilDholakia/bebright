2655f263b4c0a2d849c5be770217d8c8565f6f6f
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-10-31 11:32:20 +0000

    router added and component files made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logo from '../src/assets/BeBright-Logo.png';
 import './App.css';
+import { Route, Link, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Timeline from './components/Timeline';
+import ProfilePage from './components/ProfilePage';
+import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Timeline />} />
+          <Route exact path="/profile" element={<ProfilePage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
