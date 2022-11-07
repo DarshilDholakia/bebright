1d01bbdea48be6e87acfda6890924660330ca61a
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-11-07 00:35:30 +0000

    Added post service functionality

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import 'react-toastify/dist/ReactToastify.css';
+import authHeader from "./auth-header";
 
 const API_URL = "http://localhost:8086";
 
@@ -9,20 +9,17 @@ const addPost = (description, imageURL) => {
         .post(API_URL + "/posts", {
             description,
             imageURL,
-        })
-        .then((response) => {
-            return response.data;
-        })
+        }, 
+        { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
 };
 
 const deletePost = () => {
 
     return axios
-        .delete(API_URL + `/posts/${postId}`, {
-        })
-        .then((response) => {
-            return response.data;
-        })
+        .delete(API_URL + `/posts/${postId}`, null, { headers: authHeader() })
+        .then(() => console.log("Successfully deleted"));
 };
 
 const updatePost = (description, imageURL) => {
@@ -31,16 +28,97 @@ const updatePost = (description, imageURL) => {
         .put(API_URL + `/posts/${postId}`, {
             description,
             imageURL,
-        })
-        .then((response) => {
-            return response.data;
-        })
+        },
+        { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
+};
+
+// const getAllPosts = () => {
+
+//     return axios
+//         .get(API_URL + "/posts/all")
+//         .then((response) => {return response.data})
+//         .catch((error) => console.error(error))
+// };
+
+// const getPostById = () => {
+
+//     return axios
+//         .get(API_URL + `/posts/${postId}`)
+//         .then((response) => {return response.data})
+//         .catch((error) => console.error(error))
+// };
+
+const getPostsByUser = () => {
+
+    return axios
+        .get(API_URL + "/posts", null, { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
+};
+
+const getPostsByOffice = () => {
+
+    return axios
+        .get(API_URL + `/posts/office/${office}`, null, { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
+};
+
+const getPostsByMultipleOffices = () => {
+
+    return axios
+        .get(API_URL + "/posts/offices", null, { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
+};
+
+const getPostsByOfficeAndTeam = () => {
+
+    return axios
+        .get(API_URL + `/posts/${office}/${team}`, null, { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
+};
+
+const addLike = () => {
+
+    return axios
+        .put(API_URL + `/posts/addLike/${postId}`, null, { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
+};
+
+const removeLike = () => {
+
+    return axios
+        .put(API_URL + `/posts/removeLike/${postId}`, null, { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
+};
+
+const checkIfPostBelongsToCurrentUser = () => {
+
+    return axios
+        .get(API_URL + `/posts/check/${postId}`, null, { headers: authHeader() })
+        .then((response) => {return response.data})
+        .catch((error) => console.error(error))
 };
 
 const postService = {
     addPost,
     deletePost,
     updatePost,
+    // getAllPosts,
+    // getPostById,
+    getPostsByUser,
+    getPostsByOffice,
+    getPostsByMultipleOffices,
+    getPostsByOfficeAndTeam,
+    addLike,
+    removeLike,
+    checkIfPostBelongsToCurrentUser,
 };
 
 export default postService;
