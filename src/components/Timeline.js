ed7aba38b6a2e4d3dce78f3fdec88fbbbc6db1a4
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-11-09 00:14:11 +0000

    tried rendering image from cloud on Timeline page and it shows as it should

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,6 +5,7 @@ const Timeline = () => {
     return (
         <>
             <h1>Timeline</h1>
+            <Avatar src="https://firebasestorage.googleapis.com/v0/b/bebright-db859.appspot.com/o/files%2Fstock.jpg?alt=media&token=b1668e55-db39-49c3-83ef-ecfe41594cca" />
         </>
 
     )
