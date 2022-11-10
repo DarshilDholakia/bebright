672c826e6b4c2e5c023d4b91d2834cd6caf83323
Adib <abaig@cfcunderwriting.com>
2022-11-10 00:25:03 +0000

    fixed functions on timeline and post.js

--- src/components/Timeline/Post.js
+++ src/components/Timeline/Post.js
@@ -1,13 +1,33 @@
-import InputOption from '../InputOption';
+import InputOption from ./InputOption";
 import React, {forwardRef} from "react";
-import "../Post.css";
+import "./Post.css";
 import { Avatar } from '@material-ui/core'
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
-import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
-import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import EditIcon from '@mui/icons-material/Edit';
 
 const Post =  forwardRef (({name,description, message, photoUrl}, ref) => {
+    
+    useEffect(async () => {
+        const results = await 
+            postService.checkIfPostBelongsToCurrentUser().then()
+    
+        setPosts(results.posts)
+    }, []);
+
+    /*
+    in the backend checks if the posts belong to the user - its a boolean
+    so we need to call this end point - top right of a posts that is yours 
+    - you can see the pencil edit sign - so that you can edit your own posts yourself - if its true its only for the posts that belongs to you
+    false - if it belongs to someone else 
+
+    if true - then posts belong to user - allow to edit 
+    if false - then posts belong to user - don't allow edit 
+    return all posts in list 
+
+    end point loops through all posts 
+    */
+
     return (
         <div ref ={ref} className ='post'>
         <div className="post__header">
@@ -21,9 +41,11 @@ const Post =  forwardRef (({name,description, message, photoUrl}, ref) => {
          <div className="post__body">
              <p>{message}</p>
          </div>
+
          <div className="post__buttons">
              <InputOption Icon = {ThumbUpIcon}  title ="Like" color ="gray"/>
              <InputOption Icon = {ChatOutlinedIcon}  title ="Comment" color ="gray"/>
+             <InputOption Icon = {EditIcon}  title ="Edit" color ="gray"/>
          </div>   
         </div>
     )
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "./Timeline.css";
-import InputOption from './InputOption'
+import InputOption from "./InputOption"
 import Post from './Post'
 import CreateIcon from "@material-ui/icons/Create";
 import ImageIcon from '@material-ui/icons/Image';
@@ -12,76 +12,20 @@ import axios from 'axios';
 import postService from '../../services/post.service';
 
 export default function Timeline() {
-    const user = useSelector(selectUser)
     const [input, setInput] = useState('');
     const [posts, setPosts] = useState([]);
     
-
-    useEffect(() => {
-        db.collection("posts")
-            .orderBy("timestamp", "desc")
-            .onSnapshot((snapshot) =>
-                setPosts(snapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    data: doc.data(),
-                }))
-                )
-            );
+    useEffect(async () => {
+        await postService.getPostsByMultipleOffices().then((response) => setPosts(response))
     }, []);
 
     useEffect(async () => {
-        const results = await 
-            postService.getPostsByMultipleOffices().then()
-    
-        setPosts(results.posts)
+        await postService.getPostsByOfficeAndTeam().then((response) => setPosts(response))
     }, []);
 
     useEffect(async () => {
-        const results = await 
-            postService.getPostsByOffice().then()
-    
-        setPosts(results.posts)
-    }, []);
-
-    useEffect(async () => {
-        const results = await 
-            postService.getPostsByOfficeAndTeam().then()
-    
-        setPosts(results.posts)
-    }, []);
-
-    useEffect(async () => {
-        const results = await 
-            postService.checkIfPostBelongsToCurrentUser().then()
-    
-        setPosts(results.posts)
-    }, []);
-
-    /*
-    in the backend checks if the posts belong to the user - its a boolean
-    so we need to call this end point - top right of a posts that is yours 
-    - you can see the pencil edit sign - so that you can edit your own posts yourself - if its true its only for the posts that belongs to you
-    false - if it belongs to someone else 
-
-    if true - then posts belong to user - allow to edit 
-    if false - then posts belong to user - don't allow edit 
-    return all posts in list 
-    */
-
-
-    // useEffect(async () => {
-    //     const results = await 
-    //         postService.getPostsByUser().then()
-    
-    //     setPosts(results.posts)
-    // }, []);
-    
-
-    // useEffect(() => {
-        
-
-    // }, []);
-
+        await postService.getPostsByOffice().then((response) => setPosts(response))
+    }, []); 
 
     return (
         <div className="timeline">
@@ -90,16 +34,14 @@ export default function Timeline() {
                     <CreateIcon />
                     <form>
                         <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Start a post" />
-                        <button onClick={sendPost} type="submit">Send</button>
+                        <button onClick={postService.addPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="timeline__inputOptions">
                     <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
-                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#7FC15E" />
-                    <InputOption Icon={EventNoteIcon} title="Event" color="#E7A33E" />
-                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#F5987E" />
                 </div>
             </div>
+            
             <hr/>
 
             {/* Posts */}
